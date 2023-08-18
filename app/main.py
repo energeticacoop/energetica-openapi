f27c2b890fb71180ef3a6178cfde55b0389c022d
juaniten <juanguijarroferreiro@gmail.com>
2023-08-18 11:27:47 +0200

    fixed .env reference

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
 import mysql.connector
-
 from dotenv import dotenv_values
 
 description = """
@@ -35,7 +34,6 @@ class CheckCooperativeUserOut(BaseModel):
 def check_cooperative_member(dni: str) -> bool:
 
     db_config = dotenv_values(".env")
-    count_result = 0
     try:
         # Establish a connection to the database
         connection = mysql.connector.connect(**db_config)
