799096253a6368533ed847cd0ec768abb0be1c6b
juaniten <juanguijarroferreiro@gmail.com>
2023-08-18 14:58:25 +0200

    added metadata to endpoints

--- app/main.py
+++ app/main.py
@@ -10,6 +10,10 @@ tags_metadata = [
         "name": "users",
         "description": "Operations with users, both cooperative members and clients",
     },
+    {
+        "name": "root",
+        "description": "A useless endpoint with a message",
+    },
 ]
 
 app = FastAPI(
@@ -17,11 +21,12 @@ app = FastAPI(
     title="Energética Coop API",
     version="0.0.1",
     description=description,
+    openapi_tags=tags_metadata
 )
 
-app.include_router(users.router)
+app.include_router(users.router, tags=["users"])
 
 
-@app.get("/")
+@app.get("/", tags=["root"])
 async def read_main():
     return {"msg": "Greetings from Energética Coop!"}
