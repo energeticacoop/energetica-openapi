027dcd073056414244fb2ec64dd4d3a93f14910e
juaniten <juanguijarroferreiro@gmail.com>
2024-01-11 13:40:11 +0100

    remove pattern check for DNI/NIF/NIE

--- app/routers/users.py
+++ app/routers/users.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 
 @router.get("/check_cooperative_member/", tags=["users"], response_model=CheckCooperativeUserOut)
-async def member(dni: Annotated[str, Query(description="The DNI, NIF or NIE of a cooperative member.", pattern=r'^\d{8}[a-zA-Z]$')]) -> Any:
+async def member(dni: Annotated[str, Query(description="The DNI, NIF or NIE of a cooperative member.")]) -> Any:
     sql_query = f"SELECT COUNT(s.id) FROM `ert2_usuarios_v2` as u, `ert2_socios_v2` as s WHERE u.id=s.idusuario AND identificadorfiscal = '{dni}' AND s.estado=3;"
     result = get_db_query_result(sql_query)
     cooperative_user_exists = bool(int(result))
